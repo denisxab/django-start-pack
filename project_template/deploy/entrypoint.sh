4e702ddb25917b8418d6b08ff9ddd1f872fa3b07
“denis” <denis-kustov@rambler.ru>
2021-12-14 23:30:36 +0300

    init-1

--- project_template/deploy/entrypoint.sh
+++ project_template/deploy/entrypoint.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
+export $(cat .env | xargs) && rails c
+
 # Создать миграции
 python {{ project_name }}/manage.py makemigrations --no-input
 # Применить миграции
 python {{ project_name }}/manage.py migrate --no-input
 
-
 # Запустить сервер django
-python {{ project_name }}/manage.py runserver 0.0.0.0:8000
+python {{ project_name }}/manage.py runserver 0.0.0.0:$(EXTERNAL_WEB_PORT)
 
 # Запустить сервер gunicorn
 # gunicorn --chdir {{ project_name }} -c $(pwd)/deploy/gunicorn/gunicorn.conf.py
