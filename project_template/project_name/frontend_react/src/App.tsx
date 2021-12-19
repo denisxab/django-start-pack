3205c8a3be436794cb71a2ac295f66ae43f730b8
“denis” <denis-kustov@rambler.ru>
2021-12-19 19:52:03 +0300

    Change JS to TS

---
+++ project_template/project_name/frontend_react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+
+const App: React.FunctionComponent = () => {
+return (
+        <div>
+            Hello React + Django + Py
+        </div>
+    );
+}
+
+export default App;
--- project_template/webpack.config.js
+++ project_template/webpack.config.js
@@ -1,16 +1,18 @@
 const path = require("path");
 // Считываем переменные окружения из файла `npm install dotenv`
-const envy = require('dotenv').config({ path: './__env.env' });
+const envy = require("dotenv").config({ path: "./__env.env" });
 //Получаем имя проекта из переменных окружения
 const ProjName = envy.parsed.NAME_PROJ;
 console.log(ProjName);
 
-
+// https://webpack.js.org/guides/typescript/
 module.exports = {
   // Режим работы
   mode: "development",
+  // Включить карты кода для отладки вывода webpack
+  devtool: "inline-source-map",
 
-  entry: [path.resolve(__dirname, `${ProjName}/frontend_react/src/index.js`)],
+  entry: [path.resolve(__dirname, `${ProjName}/frontend_react/src/index.tsx`)],
   output: {
     // куда помещаются скомпилированные файлы
     path: path.resolve(
@@ -22,22 +24,29 @@ module.exports = {
     publicPath: "/static/frontend_react/public/",
     filename: "main.js", // имя для импорта в `index.html`
   },
+
+  resolve: {
+    // Добавить разрешения '.ts' и '.tsx' к обрабатываемым
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
   module: {
     // конфигурация относительно модулей
     rules: [
       {
         // проверка `regex` для `js` и `jsx` файлов
-        test: /\.(js|jsx|mjs)?$/,
+        test: /\.tsx?$/,
         // Игнорирует папки [node_modules/, bower_components/]
         exclude: /(node_modules|bower_components)/,
         // для поиска подходящих файлов используйте  `babel-loader`
-        use: {
-          loader: "babel-loader",
-          options: { presets: ["@babel/env", "@babel/preset-react"] },
-        },
+        use: 'ts-loader',
       },
     ],
   },
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM",
+  // },
   devServer: {
     devMiddleware: {
       // Записывать изменения в файл, а не в ОЗУ
